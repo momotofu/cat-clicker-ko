9c2548517755e763f29be5007ae5420261bd4517
Christopher Reece <mrkita.reece@gmail.com>
2018-04-20 18:48:38 +0900

    added knockout and jquery dependencies

--- index.js
+++ index.js
@@ -7,7 +7,51 @@ app.get('/', (req, res) => {
 })
 
 app.get('/js/:path', (req, res) => {
-  console.log('path: ', req.params.path)
   res.sendFile(path.join(`${__dirname}/js/${req.params.path}`))
 })
+
+app.get('/cats', (req, res) => {
+  res.send(JSON.stringify(
+    [
+      {
+        name: 'TigerLily',
+        url: 'https://images.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-ZHnphGtqBVI%2FTfryjXblbSI%2FAAAAAAAAEO4%2FXJb3DAuE94U%2Fs1600%2Fcat4.jpg&f=1',
+        clickCount: 0,
+        nickNames: [
+          {name: 'TigerLily'},
+          {name: 'Jessie'},
+          {name: 'Juniper'},
+          {name: 'Crisper'},
+          {name: 'Krew'}
+        ]
+      },
+      {
+        name: 'TigerLilyClone',
+        url: 'https://images.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-ZHnphGtqBVI%2FTfryjXblbSI%2FAAAAAAAAEO4%2FXJb3DAuE94U%2Fs1600%2Fcat4.jpg&f=1',
+        clickCount: 0,
+        nickNames: [
+          {name: 'TigerLily'},
+          {name: 'Jessie'},
+          {name: 'Juniper'},
+          {name: 'Crisper'},
+          {name: 'Krew'}
+        ]
+      },
+      {
+        name: 'TigerLilyImposter',
+        url: 'https://images.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-ZHnphGtqBVI%2FTfryjXblbSI%2FAAAAAAAAEO4%2FXJb3DAuE94U%2Fs1600%2Fcat4.jpg&f=1',
+        clickCount: 0,
+        nickNames: [
+          {name: 'TigerLily'},
+          {name: 'Jessie'},
+          {name: 'Juniper'},
+          {name: 'Crisper'},
+          {name: 'Krew'}
+        ]
+      },
+    ]
+  ))
+
+})
+
 app.listen(7000, () => console.log('Example app listening on port 7000!'))
