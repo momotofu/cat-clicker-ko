7ff003a6f7bdd2c1677e160762184b95c22dbfad
Christopher Reece <mrkita.reece@gmail.com>
2018-04-20 18:40:39 +0900

    app is being served successfully on a node server

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(`${__dirname}/index.html`))
 })
 
-app.get('/js/:path*', (req, res) => {
+app.get('/js/:path', (req, res) => {
+  console.log('path: ', req.params.path)
   res.sendFile(path.join(`${__dirname}/js/${req.params.path}`))
 })
 app.listen(7000, () => console.log('Example app listening on port 7000!'))
