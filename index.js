c72b325ed318b81bbdeb54709be6b8218c65e3e5
Christopher Reece <mrkita.reece@gmail.com>
2018-04-20 17:44:33 +0900

    serving up js files in html

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/index.html`))
+})
 
+app.get('/js/:path*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/js/${req.params.path}`))
+})
 app.listen(7000, () => console.log('Example app listening on port 7000!'))
