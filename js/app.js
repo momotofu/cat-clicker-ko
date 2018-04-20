fb6f242ea336dfff001a656cc9a4fe2f7412dc8d
Christopher Reece <mrkita.reece@gmail.com>
2018-04-20 16:03:43 +0900

    testing foreach binding

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 const Model = function() {
+  // attributes
   this.name = ko.observable('TigerLily');
   this.url = ko.observable('https://images.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-ZHnphGtqBVI%2FTfryjXblbSI%2FAAAAAAAAEO4%2FXJb3DAuE94U%2Fs1600%2Fcat4.jpg&f=1');
   this.clickCount = ko.observable(0);
+  this.cats = ko.observableArray([
+    {name: 'TigerLily'},
+    {name: 'Jessie'},
+    {name: 'Juniper'},
+    {name: 'Crisper'},
+    {name: 'Krew'}
+  ])
+
+  // bindings
   this.level = ko.computed(function() {
     const cur = this.clickCount()
 
