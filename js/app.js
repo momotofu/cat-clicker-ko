0e5b25215623f16239eb7df52d3dfbdebdf7373b
Christopher Reece <mrkita.reece@gmail.com>
2018-04-23 16:48:30 +0900

    sucessfully serving cat data

--- js/app.js
+++ js/app.js
@@ -1,50 +1,57 @@
-const CatModel = function(data) {
-  // attributes
-  this.name = ko.observable(data.name);
-  this.url = ko.observable(data.url);
-  this.clickCount = ko.observable(data.clickCount);
-  this.nickNames = ko.observableArray(data.nickNames)
-
-  // bindings
-  this.level = ko.computed(function() {
-    const cur = this.clickCount()
-
-     if (cur > 80) {
-      return 'ancient'
-    } else if (cur > 60) {
-      return 'adult'
-    } else if (cur > 30) {
-      return 'teen'
-    } else if (cur > 20) {
-      return 'kitten'
-    } else {
-      return 'infant'
-    }
-  }, this)
-
+async function getData() {
+  return await $.getJSON('/cats')
 }
 
-const ViewModel = function() {
-  const self = this
+getData().then(initialCats => {
+  const CatModel = function(data) {
+    // attributes
+    this.name = ko.observable(data.name);
+    this.url = ko.observable(data.url);
+    this.clickCount = ko.observable(data.clickCount);
+    this.nickNames = ko.observableArray(data.nickNames)
 
-  this.currentCat = ko.observable( new CatModel({
-    name: 'TigerLily',
-    url: 'https://images.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-ZHnphGtqBVI%2FTfryjXblbSI%2FAAAAAAAAEO4%2FXJb3DAuE94U%2Fs1600%2Fcat4.jpg&f=1',
-    clickCount: 0,
-    nickNames: [
-      {name: 'TigerLily'},
-      {name: 'Jessie'},
-      {name: 'Juniper'},
-      {name: 'Crisper'},
-      {name: 'Krew'}
-    ]
-  }))
+    // bindings
+    this.level = ko.computed(function() {
+      const cur = this.clickCount()
 
-  this.wasClicked = function() {
-    const cur = self.currentCat().clickCount() + 1
-    self.currentCat().clickCount(cur)
+       if (cur > 80) {
+        return 'ancient'
+      } else if (cur > 60) {
+        return 'adult'
+      } else if (cur > 30) {
+        return 'teen'
+      } else if (cur > 20) {
+        return 'kitten'
+      } else {
+        return 'infant'
+      }
+    }, this)
+
   }
-}
-console.log('ran')
 
-ko.applyBindings(new ViewModel)
+  const ViewModel = function() {
+    const self = this
+
+    this.catList = ko.observableArray([])
+
+    initialCats.forEach((catData) => {
+      self.catList.push(new CatModel(catData))
+    })
+
+    this.currentCat = ko.observable(this.catList()[0])
+    this.catSelected = function(clickedCat) {
+      self.currentCat(clickedCat)
+    }
+
+    this.wasClicked = function() {
+      const cur = self.currentCat().clickCount() + 1
+      self.currentCat().clickCount(cur)
+    }
+  }
+
+  ko.applyBindings(new ViewModel)
+
+}).catch(err => {
+  console.log('error: ', err)
+})
+
